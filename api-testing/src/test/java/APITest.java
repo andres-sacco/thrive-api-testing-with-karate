2b2cd6927724ba1c006503af39bf21f30b9c8640
asacco <andres.sacco@travelx.io>
2025-01-13 17:17:06 -0300

    final

--- api-testing/src/test/java/APITest.java
+++ api-testing/src/test/java/APITest.java
@@ -1,9 +1,17 @@
 import com.intuit.karate.junit5.Karate;
 
+
 class APITest {
 
     @Karate.Test
     Karate runAllTests() {
         return Karate.run().tags("~@ignore").relativeTo(getClass());
     }
+
+    /*@Test
+    void runAllParallel() {
+        Results results = Runner.path("classpath:reservation", "classpath:clusters")
+                .parallel(2); // Number of threads
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }*/
 }
